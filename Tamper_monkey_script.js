3bbc040bdae9067ea7e40e00f1d06b5ed4cb923f
农药君 <99505726+cnbilinyj@users.noreply.github.com>
2024-10-08 18:49:28 +0800

    Update Tamper monkey script

--- Tamper_monkey_script.js
+++ Tamper_monkey_script.js
@@ -26,7 +26,6 @@
 			return e;
 		})());
 		element = document.querySelector("a[class=\"mdui-btn mdui-btn-icon mdui-color-blue-400 mdui-ripple\"");
-		send_requests(0);
 	}, 300);
 	let id = window.location.search.split("?")[1].split("&")[window.location.search.split("?")[1].split("&").map(i => {
 		return i.split("=")[0].toLowerCase()
@@ -36,48 +35,67 @@
 	let urls = [
 		"https://cnbilinyj.github.io/WebCat_Shared_Space_Publicizes_Project_Direct_Chain/",
 		"https://jsd.cdn.zzko.cn/gh/cnbilinyj/WebCat_Shared_Space_Publicizes_Project_Direct_Chain@main/"
-	];
-	let no_get = function no_get () {
-		element.classList.remove("mdui-color-blue-400");
-		element.classList.add("mdui-color-red-500");
-		element.children[0].innerHTML = "downward";
-	}
-	let data = JSON.parse(localStorage.getItem("cnbilinyj-WebCat-WCSSPDLF&D")) || [];
-	let get_5 = function get_5 (urln) {
-		if (urln >= urls.length) {
-			no_get();
-			return;
+	]
+	let send_requests = function send_requests (url_n){
+		let url_ = urls[url_n];
+		if (url_ === undefined) {
+			throw new Error("Can't do it for any domain name in cnbilinyj.github.io and jsd.cdn.zzko.cn.")
 		}
-		let idn = parseInt(id.slice(0, -4));
-		if (data[idn]) {
-			/*
-			如果本地存在这一等级的记录则直接使用本地的
-			原本可以直接存储是否有当前ID的项目的下载链接
-			但是考虑到这种情况出现率较低，所以才做成查找本地同等级记录的形式
-			这样可以使得比如当前项目ID是45678，曾经从网络记录得到过45623
-			即可节省从网络请求万级(4)和百级(56)的数据
-			可以节省下这一部分的网络请求时间和次数以及网络请求所消耗的流量
-			*/
-			get_34(urln, true);
-		} else {
-			let url_ = urls[urln];
-			let get_5_xhr = new XMLHttpRequest();
-			get_5_xhr.open("GET", `${url_}index.json?timestamp=${new Date().getTime()}`, true);
-			get_5_xhr.addEventLister("load", event => {
-				let net_data = JSON.parse(event.target.responseText);
-				if (net_data[idn]) {
-					get_34(urln, false);
-				} else {
-					no_get();
-					return;
-				}
-			});
-			get_5_xhr.addEventLister("error", event => {
-				get_5(urln + 1);
-			});
-		}
+		let tenThousand_levels = new XMLHttpRequest();
+		tenThousand_levels.open("GET", `${url_}index.json?timestamp=${new Date().getTime()}`, true);
+		tenThousand_levels.addEventListener("load", event => {
+			if(JSON.parse(event.target.responseText)[parseInt(id.slice(0, -4))]){
+				let hundred_thousand = new XMLHttpRequest();
+				hundred_thousand.open("GET", `${url_}${parseInt(id.slice(0, -4))}/index.json?timestamp=${new Date().getTime()}`, true);
+				hundred_thousand.addEventListener("load", event => {
+					console.log(parseInt(id.slice(-4, -2)));
+					if(JSON.parse(event.target.responseText)[parseInt(id.slice(-4, -2))]){
+						let ten_grades = new XMLHttpRequest();
+						ten_grades.open("GET", `${url_}${parseInt(id.slice(0, -4))}/${parseInt(id.slice(-4 ,-2))}.json?timestamp=${new Date().getTime()}`, true);
+						ten_grades.addEventListener("load", event => {
+							let download_url = JSON.parse(event.target.responseText)[parseInt(id.slice(-2))];
+							element.classList.remove("mdui-color-blue-400");
+							if(download_url){
+								element.href = download_url;
+								element.classList.add("mdui-color-green-500");
+								element.children[0].innerHTML = "arrow_downward";
+							}else{
+								element.classList.add("mdui-color-red-500");
+							}
+						});
+						ten_grades.addEventListener("error", event => {
+							send_requests(url_n + 1);
+							element.classList.remove("mdui-color-blue-400");
+							element.classList.add("mdui-color-red-500");
+							element.children[0].innerHTML = "arrow_downward";
+						});
+						ten_grades.send();
+					}else{
+						element.classList.remove("mdui-color-blue-400");
+						element.classList.add("mdui-color-red-500");
+						element.children[0].innerHTML = "arrow_downward";
+					}
+				});
+				hundred_thousand.addEventListener("error", event => {
+					send_requests(url_n + 1);
+					element.classList.remove("mdui-color-blue-400");
+					element.classList.add("mdui-color-red-500");
+					element.children[0].innerHTML = "arrow_downward";
+				});
+				hundred_thousand.send();
+			}else{
+				element.classList.remove("mdui-color-blue-400");
+				element.classList.add("mdui-color-red-500");
+				element.children[0].innerHTML = "arrow_downward";
+			}
+		});
+		tenThousand_levels.addEventListener("error", event => {
+			send_requests(url_n + 1);
+			element.classList.remove("mdui-color-blue-400");
+			element.classList.add("mdui-color-red-500");
+			element.children[0].innerHTML = "arrow_downward";
+		});
+		tenThousand_levels.send();
 	}
-	let get_34 = function get_34 (urln, get_local) {
-		if (get_local && data[parseInt(id.slice(0, -4))])
-	}
+	send_requests(0);
 })();
