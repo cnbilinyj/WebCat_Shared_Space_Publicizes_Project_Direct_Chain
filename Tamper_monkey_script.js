d5225ff649de1ac9ec14b1ccee7cc27b8741044d
农药君 <99505726+cnbilinyj@users.noreply.github.com>
2024-10-08 16:16:31 +0800

    Update Tamper monkey script

--- Tamper_monkey_script.js
+++ Tamper_monkey_script.js
@@ -10,6 +10,23 @@
 // ==/UserScript==
 (function() {
 	'use strict';
+	let element = document.querySelector("button[class=\"mdui-btn mdui-btn-icon mdui-color-pink-400 mdui-ripple\"").parentNode;
+	document.querySelector("button[class=\"mdui-btn mdui-btn-icon mdui-color-pink-400 mdui-ripple\"").remove();
+	setTimeout(() => {
+		element.appendChild((() => {
+			let e = document.createElement("a");
+			e.setAttribute("class", "mdui-btn mdui-btn-icon mdui-color-blue-400 mdui-ripple");
+			e.href = "javascript:vue.download()";
+			e.appendChild((() => {
+				let e = document.createElement("i");
+				e.setAttribute("class", "mdui-icon material-icons");
+				e.innerHTML = "more";
+				return e;
+			})());
+			return e;
+		})());
+	}, 100);
+	element = document.querySelector("a[class=\"mdui-btn mdui-btn-icon mdui-color-blue-400 mdui-ripple\"");
 	let id = window.location.search.split("?")[1].split("&")[window.location.search.split("?")[1].split("&").map(i => {
 		return i.split("=")[0].toLowerCase()
 	}).indexOf("id")].split("=")[1].padStart(5, "0");
@@ -37,35 +54,33 @@
 						ten_grades.open("GET", `${url_}${parseInt(id.slice(0, -4))}/${parseInt(id.slice(-4 ,-2))}.json?timestamp=${new Date().getTime()}`, true);
 						ten_grades.addEventListener("load", event => {
 							let download_url = JSON.parse(event.target.responseText)[parseInt(id.slice(-2))];
+							element.classList.remove("mdui-color-blue-400");
 							if(download_url){
-								let element = document.querySelector("button[class=\"mdui-btn mdui-btn-icon mdui-color-pink-400 mdui-ripple\"").parentNode;
-								document.querySelector("button[class=\"mdui-btn mdui-btn-icon mdui-color-pink-400 mdui-ripple\"").remove();
-								setTimeout(() => {
-									element.appendChild((() => {
-										let e = document.createElement("a");
-										e.setAttribute("class", "mdui-btn mdui-btn-icon mdui-color-green-400 mdui-ripple");
-										e.href = download_url;
-										e.appendChild((() => {
-											let e = document.createElement("i");
-											e.setAttribute("class", "mdui-icon material-icons");
-											e.innerHTML = "arrow_downward"
-											return e;
-										})());
-										return e;
-									})());
-								}, 100);
+								element.href = download_url;
+								element.classList.add("mdui-color-green-500");
+								element.children[0].innerHTML = "arrow_downward";
+							}else{
+								element.classList.add("mdui-color-red-500");
 							}
 						});
 						ten_grades.addEventListener("error", event => {
 							send_requests(url_n + 1);
 						});
 						ten_grades.send();
+					}else{
+						element.classList.remove("mdui-color-blue-400");
+						element.classList.add("mdui-color-red-500");
+						element.children[0].innerHTML = "arrow_downward";
 					}
 				});
 				hundred_thousand.addEventListener("error", event => {
 					send_requests(url_n + 1);
 				});
 				hundred_thousand.send();
+			}else{
+				element.classList.remove("mdui-color-blue-400");
+				element.classList.add("mdui-color-red-500");
+				element.children[0].innerHTML = "arrow_downward";
 			}
 		});
 		tenThousand_levels.addEventListener("error", event => {
